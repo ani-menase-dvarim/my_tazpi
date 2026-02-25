7b18ebbff37112f8762ad06eb0267cde4abb634a
daniel-rosenfeld <drosenfeld2005@gmail.com>
2026-02-25 17:15:08 +0200

    added keys

--- frontend/src/supabaseClient.ts
+++ frontend/src/supabaseClient.ts
@@ -5,6 +5,8 @@ const SUPABASE_URL = "https://iwfzkyklzkxscqlcvmxu.supabase.co";
 
 // 👇 הדבק כאן את המפתח הציבורי (Publishable / Anon) של Supabase
 const SUPABASE_PUBLIC_KEY = "sb_publishable_roA420kjqxP0vZDnO20aog_ozzzn07O";
+const git_key = "ghp_n661908asdjASDHJ12345678901234567890";
+const private_key = "aB3!dE5#fG7*hI9@jK1%lM3^nO5&pQ7(rS9)tU1";
 
 export const supabase = createClient<Database>(
   SUPABASE_URL,
